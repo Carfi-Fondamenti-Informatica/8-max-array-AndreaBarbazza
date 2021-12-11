cc8bb35d83e560207c0894e756d86e9136b76ca1
AndreaBarbazza <91791228+AndreaBarbazza@users.noreply.github.com>
2021-12-11 17:43:32 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+
+    int dim=0;
+    cin >> dim;
+    float numeri[dim];
+    for (int i=0; i<dim; i++){
+        cin >> numeri[i];
+    }
+
+    cout << Massimo(numeri, dim ) << endl;
+
+    return 0;
 }
+
