3d64c51d1e6fa491210cc04aee2db57c89c4bfe9
AndreaBarbazza <91791228+AndreaBarbazza@users.noreply.github.com>
2021-12-11 17:40:08 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
+#include "lib.h"
+#include <iostream>
 
+
+float Massimo (float numeri[], int dimensione){
+
+    float max=numeri[0];
+    for (int i=0; i<dimensione; i++){
+        if(numeri[i]>max){
+            max=numeri[i];
+        }
+    }
+    return max;
+}
